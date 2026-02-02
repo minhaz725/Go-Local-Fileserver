3d80e176e963367cca81648f9a0c3888b3087c20
Minhaz <minhaz725@gmail.com>
2026-02-03 04:38:59 +0600

    updated ip logic

--- fileserver.go
+++ fileserver.go
@@ -1,55 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net"
-    "net/http"
-    "os"
-    "strings"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
-// Get the local IP address of the machine
 func getLocalIP() string {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        return "localhost"
-    }
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "localhost"
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ip := ipnet.IP.To4()
+			// Skip link-local addresses (169.254.x.x)
+			if ip[0] == 169 && ip[1] == 254 {
+				continue
+			}
+			return ipnet.IP.String()
+		}
+	}
+	return "localhost (not connected to network)"
+}
 
-    for _, addr := range addrs {
-        if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-            if ipnet.IP.To4() != nil {
-                // Skip virtual interfaces like VirtualBox, VMware, etc.
-                if !strings.Contains(ipnet.IP.String(), "169.254") && // Skip link-local
-                   !strings.Contains(ipnet.IP.String(), "192.168.56") { // Skip VirtualBox
-                    return ipnet.IP.String()
-                }
-            }
-        }
-    }
-    return "localhost"
+func noCacheMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-    shareDir := "./shared"
-    
-    // Create the directory if it doesn't exist
-    if _, err := os.Stat(shareDir); os.IsNotExist(err) {
-        os.Mkdir(shareDir, 0755)
-        fmt.Printf("Created directory: %s\n", shareDir)
-    }
-    
-    // File server handler
-    fs := http.FileServer(http.Dir(shareDir))
-    http.Handle("/", fs)
-    
-    port := "8080"
-    localIP := getLocalIP()
-    
-    fmt.Printf("File server running at:\n")
-    fmt.Printf("Local: http://localhost:%s\n", port)
-    fmt.Printf("Network: http://%s:%s\n", localIP, port)
-    fmt.Printf("Serving files from: %s\n", shareDir)
-    
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	shareDir := "./shared"
+	port := "8080"
+
+	os.MkdirAll(shareDir, 0755)
+
+	http.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "http://%s:%s", getLocalIP(), port)
+	})
+
+	http.Handle("/", noCacheMiddleware(http.FileServer(http.Dir(shareDir))))
+
+	fmt.Printf("File server running on port %s\n", port)
+	fmt.Printf("Local:   http://localhost:%s\n", port)
+	fmt.Printf("Network: http://%s:%s\n", getLocalIP(), port)
+	fmt.Printf("Current IP endpoint: /ip\n")
+	fmt.Printf("Serving: %s\n", shareDir)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
