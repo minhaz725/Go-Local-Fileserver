a21c9a95183deb9ffe8395f13543489bfb5e72d3
Minhaz <minhaz725@gmail.com>
2026-02-03 04:49:56 +0600

    fixed bug

--- fileserver.go
+++ fileserver.go
@@ -8,22 +8,31 @@ import (
 	"os"
 )
 
-func getLocalIP() string {
+func getLocalIPs() []string {
+	var ips []string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return "localhost"
+		return ips
 	}
 	for _, addr := range addrs {
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
 			ip := ipnet.IP.To4()
-			// Skip link-local addresses (169.254.x.x)
+			// Skip link-local (169.254.x.x)
 			if ip[0] == 169 && ip[1] == 254 {
 				continue
 			}
-			return ipnet.IP.String()
+			// Skip VirtualBox (192.168.56.x)
+			if ip[0] == 192 && ip[1] == 168 && ip[2] == 56 {
+				continue
+			}
+			// Skip common VPN ranges (10.x.x.x)
+			if ip[0] == 10 {
+				continue
+			}
+			ips = append(ips, ipnet.IP.String())
 		}
 	}
-	return "localhost (not connected to network)"
+	return ips
 }
 
 func noCacheMiddleware(h http.Handler) http.Handler {
@@ -41,17 +50,21 @@ func main() {
 
 	os.MkdirAll(shareDir, 0755)
 
-	http.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "http://%s:%s", getLocalIP(), port)
-	})
-
 	http.Handle("/", noCacheMiddleware(http.FileServer(http.Dir(shareDir))))
 
-	fmt.Printf("File server running on port %s\n", port)
+	ips := getLocalIPs()
+
+	fmt.Println("========================================")
+	fmt.Println("        Simple File Server")
+	fmt.Println("========================================")
 	fmt.Printf("Local:   http://localhost:%s\n", port)
-	fmt.Printf("Network: http://%s:%s\n", getLocalIP(), port)
-	fmt.Printf("Current IP endpoint: /ip\n")
-	fmt.Printf("Serving: %s\n", shareDir)
+	if len(ips) > 0 {
+		fmt.Printf("Network: http://%s:%s\n", ips[0], port)
+	} else {
+		fmt.Println("Network: Not connected to local network")
+	}
+	fmt.Printf("Folder:  %s\n", shareDir)
+	fmt.Println("========================================")
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
